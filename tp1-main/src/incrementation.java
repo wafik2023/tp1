0104592eb1e1b3d6ba7df9e27afce4e1dd5659e9
wafik2023 <153209884+wafik2023@users.noreply.github.com>
2023-12-07 16:31:57 +0100

    Add files via upload

---
+++ tp1-main/src/incrementation.java
@@ -0,0 +1,29 @@
+public class incrementation {
+    public static void main(String[] args) {
+        int i,j,n;
+        i=0;
+        n=i++;
+        System.out.println("A:i="+i+"n="+n);
+        i=10;
+        n=++i;
+        System.out.println("B:i="+i+"n="+n);
+        i=20;
+        j=5;
+        n=i++*++j;
+        System.out.println("c:i="+i+"j="+j+"n="+n);
+        i=15;
+        n=i +=3;
+        System.out.println("D:i="+i+"n="+n);
+
+        i=3;
+        j=5;
+        n=i+= --j;
+        System.out.println("E:i="+i+"j="+j+"n="+n);
+
+
+
+
+    }
+
+
+}
